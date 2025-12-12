54cb63522cd0b0a69424666f40b91343d21f23fd
Gank <santya.2003.06@gmail.com>
2025-12-12 17:43:58 -0600

    feature: DRF was installed and registered

--- amazon_ads_backend/config/settings.py
+++ amazon_ads_backend/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'campaigns'
+    'rest_framework',
+    'campaigns',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+    ],
+}
